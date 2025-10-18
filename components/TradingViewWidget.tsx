8155a967c5e7e081cc6dbbe00445d04f804c2e99
SSY199 <s.s.y.9321423@gmail.com>
2025-10-18 18:40:29 +0530

    Implement home page with TradingView widget

---
+++ components/TradingViewWidget.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import UseTradingViewWidget from "@/hooks/UseTradingViewWidget";
+import React, { useRef, memo } from "react";
+import { cn } from "@/lib/utils";
+
+interface TradingViewWidgetProps {
+  title?: string;
+  scriptUrl: string;
+  config: Record<string, unknown>;
+  height?: number;
+  className?: string;
+}
+
+const TradingViewWidget = ({
+  title,
+  scriptUrl,
+  config,
+  height = 600,
+  className,
+}: TradingViewWidgetProps) => {
+  const container = UseTradingViewWidget(scriptUrl, config, height);
+
+  return (
+    <div className="w-full">
+      {title && (
+        <h3 className="font-semibold text-2xl text-gray-100 mb-5">{title}</h3>
+      )}
+
+      <div
+        className={cn("tradingview-widget-container", className)}
+        ref={container}
+      >
+        <div
+          className="tradingview-widget-container__widget"
+          style={{ height, width: "100%" }}
+        />
+        
+      </div>
+    </div>
+  );
+};
+
+export default memo(TradingViewWidget);
